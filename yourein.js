1a03276cab2e029821b30d5eefe8f09ea2d768b7
Maekl <michael.thee.michael@gmail.com>
2023-09-02 00:31:34 -0400

    k

--- yourein.js
+++ yourein.js
@@ -81,3 +81,16 @@ else{
 tryAgain();
 return false;
 }};
+const span = document.querySelector("span");
+
+span.onclick = function() {
+  document.execCommand("copy");
+}
+
+span.addEventListener("copy", function(event) {
+  event.preventDefault();
+  if (event.clipboardData) {
+    event.clipboardData.setData("text/plain", span.textContent);
+    console.log(event.clipboardData.getData("text"))
+  }
+});
